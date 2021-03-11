2526af34c92fc79d08f96a8c1510c89b47e2ae4d
Milan Košťák <milankostak@seznam.cz>
2021-03-12 00:01:11 +0100

    Lecture06 (layouts) initial commit

---
+++ Lecture06/app/src/main/java/edu/kcg/mobile/layouts/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,48 @@
+package edu.kcg.mobile.layouts.ui.main
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import edu.kcg.mobile.layouts.R
+import edu.kcg.mobile.layouts.ui.main.fragments.ConstraintFragment
+import edu.kcg.mobile.layouts.ui.main.fragments.LinearFragment
+import edu.kcg.mobile.layouts.ui.main.fragments.RelativeFragment
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_linear,
+    R.string.tab_relative,
+    R.string.tab_constraint
+)
+
+const val ARG_SECTION_NUMBER = "section_number"
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> LinearFragment()
+            1 -> RelativeFragment()
+            else -> ConstraintFragment()
+        }.apply {
+            arguments = Bundle().apply {
+                putInt(ARG_SECTION_NUMBER, position)
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return TAB_TITLES.size
+    }
+
+}
---
+++ Lecture06/app/src/main/java/edu/kcg/mobile/layouts/ui/main/fragments/ConstraintFragment.kt
@@ -0,0 +1,39 @@
+package edu.kcg.mobile.layouts.ui.main.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import edu.kcg.mobile.layouts.R
+import edu.kcg.mobile.layouts.ui.main.ARG_SECTION_NUMBER
+import edu.kcg.mobile.layouts.ui.main.PageViewModel
+
+class ConstraintFragment : Fragment() {
+
+    private lateinit var pageViewModel: PageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_constraint, container, false)
+        val textView: TextView = root.findViewById(R.id.message_constraint)
+        pageViewModel.text.observe(this, Observer<String> {
+            textView.text = it
+        })
+        return root
+    }
+
+}
