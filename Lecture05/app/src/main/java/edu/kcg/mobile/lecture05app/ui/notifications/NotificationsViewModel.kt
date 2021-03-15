9d52a6e9a5b0f2529d126faefed06c93abd3ecd9
Milan Košťák <milankostak@seznam.cz>
2021-03-15 15:24:03 +0100

    Lecture05 (bottom navigation template) initial commit

---
+++ Lecture05/app/src/main/java/edu/kcg/mobile/lecture05app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,14 @@
+package edu.kcg.mobile.lecture05app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+
+}
